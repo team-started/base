a9863bf06270b12df468f1d897b7950b9ab68bcb
Christian Wolfram <c.wolfram@chriwo.de>
2022-01-16 22:08:40 +0100

    [TASK][CLEANUP] Fix code style with php-cs-fixer and TYPO3 coding standard

--- app/backend/.php-cs-fixer.php
+++ app/backend/.php-cs-fixer.php
@@ -4,4 +4,10 @@ $config = \TYPO3\CodingStandards\CsFixerConfig::create();
 $config
     ->getFinder()
     ->in(__DIR__ . '/packages');
+
+// add own rule configuration
+$rules = $config->getRules();
+$rules['single_blank_line_before_namespace'] = true;
+$config->setRules($rules);
+
 return $config;
