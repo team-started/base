b0360b161a72c6363886e89c3421b591c17bd3ac
Marc Fell <info@marc-fell.de>
2022-03-22 07:56:48 +0000

    [FEATURE] #64 Fractal: general config für Module, wo immer gleicher Context verwaltet wird

--- app/backend/packages/starter_freya/frontend/src/ui/3-modules/m02-text/m02-text.config.js
+++ app/backend/packages/starter_freya/frontend/src/ui/3-modules/m02-text/m02-text.config.js
@@ -1,49 +1,12 @@
 'use strict';
 
 const _ = require('../../../../config/helpers');
+const globalConfig = require('../module-general.config');
 
 module.exports = {
     context: {
-        bodytext: _.lorem(500, 'bodytext: '),
+        ...globalConfig,
         CType: 'text',
-        date: '23.02.2022',
-        header: {
-            header: _.lorem(50, 'header: '),
-            header_layout: '',
-            header_link: '',
-            header_position: '',
-            tx_starter_headercolor: 'text-primary',
-            tx_starter_headerfontsize: '',
-        },
-        layout: '',
-        overline: {
-            header_position: '',
-            tx_starter_overline: _.lorem(50, 'overline: '),
-        },
-        space_after_class: '',
-        space_before_class: '',
-        subheader: {
-            header_position: '',
-            subheader: _.lorem(50, 'subheader: '),
-        },
-        uid: 999,
-        tx_starter_background: '',
-        tx_starter_background_fluid: false,
-        tx_starter_bordercolor: '',
-        tx_starter_container: '',
-        tx_starter_cta_link: {
-            config: {
-                uri: '#',
-                target: '',
-                class: 'info2',
-                title: '',
-            },
-        },
-        tx_starter_cta_text: _.lorem(20, 'max 20'),
-        tx_starter_showbyscreensize: '',
-        tx_starter_textclass: '',
-        tx_starter_textcolor: '',
-        tx_starter_width: '',
     },
     variants: [
         {
