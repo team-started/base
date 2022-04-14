4e012e001f5371f704b31ac37534175728fdca0f
Marc Fell <info@marc-fell.de>
2022-04-14 10:42:35 +0200

    cleanup fractal config.js

--- app/backend/packages/starter_freya/frontend/src/ui/3-modules/m03-textmedia/m03-textmedia.config.js
+++ app/backend/packages/starter_freya/frontend/src/ui/3-modules/m03-textmedia/m03-textmedia.config.js
@@ -1,6 +1,5 @@
 'use strict';
 
-const _ = require('../../../../config/helpers');
 const globalConfig = require('../module-general.config');
 const textmediaConfig = require('../../2-components/textmedias/textmedia.config');
 
