7b82e884b632edda0a987859650621e65da6a974
Marc Fell <info@marc-fell.de>
2022-04-21 14:37:30 +0200

    icon

--- app/backend/packages/starter_freya/frontend/src/ui/1-objects/index.js
+++ app/backend/packages/starter_freya/frontend/src/ui/1-objects/index.js
@@ -1,6 +1,5 @@
 import { registerComponent } from '@/js/utils';
 
-import icon, { className as iconClassName } from '../0-playground/p-icons/index';
 import picture, { className as pictureClassName } from './pictures/index';
 // import object, { className as objectClassName } from './objects/index';
 // import objectCta, { className as objectCtaClassName } from './objects/object-cta/index';
@@ -9,7 +8,6 @@ import picture, { className as pictureClassName } from './pictures/index';
 const objects = [
     // { comp: object, class: objectClassName },
     // { comp: objectCta, class: objectCtaClassName },
-    { comp: icon, class: iconClassName },
     { comp: picture, class: pictureClassName },
     // and so on...
 ];
--- app/backend/packages/starter_freya/frontend/src/ui/2-components/icons/icon.config.js
+++ app/backend/packages/starter_freya/frontend/src/ui/2-components/icons/icon.config.js
@@ -5,14 +5,51 @@ const _ = require('../../../../config/helpers');
 module.exports = {
     context: {
         image: '@image--svg',
+        layout: '',
         link: {
             config: {
                 uri: '#',
-                target: '',
+                target: '_blank',
                 class: '',
                 title: '',
             },
         },
-        text: _.lorem(20, 'Icon Text: '),
+        text: '',
     },
+    variants: [
+        {
+            name: 'image-only',
+            context: {
+                link: '',
+            },
+        },
+        {
+            name: 'text-right',
+            context: {
+                layout: 'text-right',
+                text: _.lorem(20, 'Text right:'),
+            },
+        },
+        {
+            name: 'text-bottom',
+            context: {
+                layout: 'text-bottom',
+                text: _.lorem(20, 'Text bottom:'),
+            },
+        },
+        {
+            name: 'text-left',
+            context: {
+                layout: 'text-left',
+                text: _.lorem(20, 'Text left:'),
+            },
+        },
+        {
+            name: 'text-top',
+            context: {
+                layout: 'text-top',
+                text: _.lorem(20, 'Text top:'),
+            },
+        },
+    ],
 };
