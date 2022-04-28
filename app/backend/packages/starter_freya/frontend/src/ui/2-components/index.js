564ca41491cb9c09d3406f691d6c9d06c011cab5
Marc Fell <info@marc-fell.de>
2022-04-28 14:30:00 +0000

    [CLEANUP][#83] Nicht benötigte Teaser-Komponente entfernt

--- app/backend/packages/starter_freya/frontend/src/ui/2-components/index.js
+++ app/backend/packages/starter_freya/frontend/src/ui/2-components/index.js
@@ -3,13 +3,11 @@ import { registerComponent } from '@/js/utils';
 import accordion, { className as accordionClassName } from './accordions/index';
 // import component, { className as componentClassName } from './component/index';
 import navmmenu, { className as navmmenuClassName } from './navigation/navmmenus/index';
-import teaser, { className as teaserClassName } from './teasers/index';
 
 const components = [
     { comp: accordion, class: accordionClassName },
     // { comp: component, class: componentClassName },
     { comp: navmmenu, class: navmmenuClassName },
-    { comp: teaser, class: teaserClassName },
 ];
 
 // Do not touch
