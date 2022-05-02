3b96bf4774d93e142b55953b5768aaa99da6b831
Marc Fell <info@marc-fell.de>
2022-05-02 14:38:32 +0200

    breadcrumb funktioniert initial

---
+++ app/backend/packages/starter_freya/frontend/src/ui/2-components/breadcrumbs/breadcrumb.config.js
@@ -0,0 +1,45 @@
+'use strict';
+
+module.exports = {
+    label: 'Breadcrumb',
+    context: {
+        items: [
+            {
+                uid: 1,
+                title: 'Rootseite',
+                link: '#',
+                target: '',
+                active: 1,
+                current: 0,
+                spacer: 0,
+            },
+            {
+                uid: 2,
+                title: 'Seite 1',
+                link: '#',
+                target: '',
+                active: 1,
+                current: 0,
+                spacer: 0,
+            },
+            {
+                uid: 3,
+                title: 'Unterseite 1.1',
+                link: '#',
+                target: '',
+                active: 1,
+                current: 0,
+                spacer: 0,
+            },
+            {
+                uid: 4,
+                title: 'Unterseite 1.1.1, aktuelle Seite',
+                link: '#',
+                target: '',
+                active: 1,
+                current: 1,
+                spacer: 0,
+            },
+        ],
+    },
+};
---
+++ app/backend/packages/starter_freya/frontend/src/ui/2-components/breadcrumbs/readme.md
@@ -0,0 +1,7 @@
+# Breadcrumb
+
+[Bootstrap Dokumentation](https://getbootstrap.com/docs/5.1/components/breadcrumb/) zum Theme Container.
+
+- items.uid: page uid
+- items.active: trifft auf alle items zu (rootline)
+- items.current: das letzte Item der breadcrumb, also die aktuelle Seite
