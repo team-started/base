8479e8e868fcd05eb908a6cbb204e116feb9510b
Marc Fell <info@marc-fell.de>
2022-05-05 14:28:05 +0200

    wip

---
+++ app/backend/packages/starter_freya/frontend/src/ui/2-components/cards/card.config.js
@@ -0,0 +1,50 @@
+'use strict';
+
+const _ = require('../../../../config/helpers');
+
+module.exports = {
+    context: {
+        uid: '999',
+        category: '',
+        date: '',
+        header: _.lorem(50, 'header:'),
+        image: '@picture--card',
+        link: {
+            config: {
+                uri: '',
+                target: '',
+                class: '',
+                title: '',
+            },
+        },
+        link_text: '',
+        subheader: '',
+        text: _.lorem(200, 'text: '),
+    },
+    variants: [
+        {
+            name: 'image',
+            context: {
+                image: '@picture--card',
+            },
+        },
+        {
+            name: 'full-set',
+            context: {
+                category: _.lorem(20, 'category:'),
+                date: '05.05.2022',
+                image: '@picture--card',
+                link: {
+                    config: {
+                        uri: '#',
+                        target: '',
+                        class: '',
+                        title: '',
+                    },
+                },
+                link_text: _.lorem(20, 'link_text: '),
+                subheader: _.lorem(50, 'subheader: '),
+            },
+        },
+    ],
+};
