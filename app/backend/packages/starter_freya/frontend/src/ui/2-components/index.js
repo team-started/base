48a178a4e04dcc49b61c7301472436a40e347d94
Marc Fell <info@marc-fell.de>
2022-05-15 22:48:35 +0200

    linting

--- app/backend/packages/starter_freya/frontend/src/ui/2-components/index.js
+++ app/backend/packages/starter_freya/frontend/src/ui/2-components/index.js
@@ -2,9 +2,9 @@ import './accordions/index';
 
 import { registerComponent } from '@/js/utils';
 
-import swiper, { className as swiperClassName } from './swipers/index';
 // import component, { className as componentClassName } from './component/index';
 import navmmenu, { className as navmmenuClassName } from './navigation/navmmenus/index';
+import swiper, { className as swiperClassName } from './swipers/index';
 
 const components = [
     // { comp: component, class: componentClassName },
