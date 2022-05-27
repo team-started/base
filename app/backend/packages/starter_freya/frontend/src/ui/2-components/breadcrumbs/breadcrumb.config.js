73747c27f349f30ca15ef7108bd7962b40a8a161
Marc Fell <info@marc-fell.de>
2022-05-27 18:36:09 +0200

    Datenstruktur korrigiert

--- app/backend/packages/starter_freya/frontend/src/ui/2-components/breadcrumbs/breadcrumb.config.js
+++ app/backend/packages/starter_freya/frontend/src/ui/2-components/breadcrumbs/breadcrumb.config.js
@@ -4,40 +4,48 @@ module.exports = {
     context: {
         items: [
             {
-                uid: 1,
+                data: {
+                    uid: 1,
+                    doktype: 1,
+                },
                 title: 'Rootseite',
                 link: '#',
                 target: '',
                 active: 1,
                 current: 0,
-                spacer: 0,
             },
             {
-                uid: 2,
+                data: {
+                    uid: 2,
+                    doktype: 1,
+                },
                 title: 'Seite 1',
                 link: '#',
                 target: '',
                 active: 1,
                 current: 0,
-                spacer: 0,
             },
             {
-                uid: 3,
+                data: {
+                    uid: 3,
+                    doktype: 1,
+                },
                 title: 'Unterseite 1.1',
                 link: '#',
                 target: '',
                 active: 1,
                 current: 0,
-                spacer: 0,
             },
             {
-                uid: 4,
+                data: {
+                    uid: 4,
+                    doktype: 1,
+                },
                 title: 'Unterseite 1.1.1, aktuelle Seite',
                 link: '#',
                 target: '',
                 active: 1,
                 current: 1,
-                spacer: 0,
             },
         ],
     },
