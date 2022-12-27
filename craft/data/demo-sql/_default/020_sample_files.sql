299a56c88330dd2f3ae68a99b10bc5472e2a47d3
Christian Wolfram <c.wolfram@chriwo.de>
2022-12-27 12:18:35 +0000

    [FEATURE][#108] Integrate new content element CE-Portfolio FileCollections

--- craft/data/demo-sql/_default/020_sample_files.sql
+++ craft/data/demo-sql/_default/020_sample_files.sql
@@ -5,11 +5,35 @@ VALUES
 	(1, 1645446125, 1, '2', '/user_upload/_default/images/cole-freeman-NHXy97995Sg-unsplash.jpg', '6b8a271071921ec7011482273cdca251aaab8d1f', '480f9128d514071ced0c62bf01be7e789c618893', 'jpg', 'image/jpeg', 'cole-freeman-NHXy97995Sg-unsplash.jpg', 'dca3e252e0a763ad2490fb2df0e596580b179e31', 2832700, 1645446125, 1645436845),
 	(2, 1645446125, 1, '2', '/user_upload/_default/images/daniel-santiago-46OsJaeD1F0-unsplash.jpg', '99d54d57239eb835ee2c01cd876cc153ae48fe0b', '480f9128d514071ced0c62bf01be7e789c618893', 'jpg', 'image/jpeg', 'daniel-santiago-46OsJaeD1F0-unsplash.jpg', '94b605ded2b8ac60e06035501741433179ad0ebf', 745428, 1645446125, 1645436845),
 	(3, 1645446125, 1, '2', '/user_upload/_default/images/eugene-chystiakov-TCQmflzrZRQ-unsplash.jpg', 'b8ea223ceb80ea7a7e14e2e6444a1f745bc173ab', '480f9128d514071ced0c62bf01be7e789c618893', 'jpg', 'image/jpeg', 'eugene-chystiakov-TCQmflzrZRQ-unsplash.jpg', 'd51c577ade7f78ecef0c18dc0655f6fc5ca4e146', 1171362, 1645446125, 1645436845),
-	(4, 1645446125, 1, '2', '/user_upload/_default/images/ruta-gudeliene-MkPt_FzqSkI-unsplash.jpg', 'b447331f905140b34613b82e48cb7fbae82f1e8e', '480f9128d514071ced0c62bf01be7e789c618893', 'jpg', 'image/jpeg', 'ruta-gudeliene-MkPt_FzqSkI-unsplash.jpg', '377cf7548339553e4101eb9461c92ddc7017b116', 5467496, 1645446125, 1645436846);
+	(4, 1645446125, 1, '2', '/user_upload/_default/images/ruta-gudeliene-MkPt_FzqSkI-unsplash.jpg', 'b447331f905140b34613b82e48cb7fbae82f1e8e', '480f9128d514071ced0c62bf01be7e789c618893', 'jpg', 'image/jpeg', 'ruta-gudeliene-MkPt_FzqSkI-unsplash.jpg', '377cf7548339553e4101eb9461c92ddc7017b116', 5467496, 1645446125, 1645436846),
+	(5, 1669360732, 1, '2', '/user_upload/_default/images/coffee/ante-samarzija-lsmu0rUhUOk-unsplash.jpg', 'd246e4916d1502a97898ca32f87946f15995cb4a', '5fcdd6b05f77f18c38c3a5de68e97d04b609cfd1', 'jpg', 'image/jpeg', 'ante-samarzija-lsmu0rUhUOk-unsplash.jpg', '7897296d0d14d4835a99a6a627ee163cc82a38da', 2168009, 1669360711, 1669360229),
+	(6, 1669360732, 1, '2', '/user_upload/_default/images/coffee/jeanie-de-klerk-hmLY7GiNFyE-unsplash.jpg', '8ee0516c9974cbe86c5438088d0b37a3c8354e54', '5fcdd6b05f77f18c38c3a5de68e97d04b609cfd1', 'jpg', 'image/jpeg', 'jeanie-de-klerk-hmLY7GiNFyE-unsplash.jpg', '6654e1278dc31bf19bbffd460b8317f69b4e0de9', 3079693, 1669360711, 1669360395),
+	(7, 1669360732, 1, '2', '/user_upload/_default/images/coffee/jeremy-yap-jn-HaGWe4yw-unsplash.jpg', '8014734e360b935d45bb0c02e9477902a96ef15c', '5fcdd6b05f77f18c38c3a5de68e97d04b609cfd1', 'jpg', 'image/jpeg', 'jeremy-yap-jn-HaGWe4yw-unsplash.jpg', '1fb2ce33d137c5ab96a18a036ad7adad86d69607', 2406740, 1669360711, 1669360191),
+	(8, 1669360732, 1, '2', '/user_upload/_default/images/coffee/nathan-dumlao-l59fmhtprIE-unsplash.jpg', '0da6b318221d6d8c8e935d21fc95bc0b6ac09d96', '5fcdd6b05f77f18c38c3a5de68e97d04b609cfd1', 'jpg', 'image/jpeg', 'nathan-dumlao-l59fmhtprIE-unsplash.jpg', '5d579e7f3e59494e45cc9dc443c4a058476e70d4', 602566, 1669360711, 1669360566),
+	(9, 1669360732, 1, '2', '/user_upload/_default/images/coffee/nathan-dumlao-mgxgvwam_-c-unsplash.jpg', 'a4a13ceabae2207bf1893c105b426f08ce620fd7', '5fcdd6b05f77f18c38c3a5de68e97d04b609cfd1', 'jpg', 'image/jpeg', 'nathan-dumlao-mgxgvwam_-c-unsplash.jpg', '7e90c092d5a18f5c58f76ab5ae143a5a6b01ead1', 2513388, 1669360711, 1669360299),
+	(10, 1669360732, 1, '2', '/user_upload/_default/images/coffee/nathan-dumlao-nBJHO6wmRWw-unsplash.jpg', '001cafebd81e36c8e3620e7577cca7e286e7dd3c', '5fcdd6b05f77f18c38c3a5de68e97d04b609cfd1', 'jpg', 'image/jpeg', 'nathan-dumlao-nBJHO6wmRWw-unsplash.jpg', '2c442c41bcf1598598c57b748792366b63fb676a', 2057973, 1669360711, 1669360102),
+	(11, 1669360732, 1, '2', '/user_upload/_default/images/coffee/olivia-anne-snyder-R5RtlMOYgX8-unsplash.jpg', 'b94bb2b367cd5375f9268c7b50c4b15a1ccc4ee7', '5fcdd6b05f77f18c38c3a5de68e97d04b609cfd1', 'jpg', 'image/jpeg', 'olivia-anne-snyder-R5RtlMOYgX8-unsplash.jpg', '66e7903b9ddbcfb39804ad597500fc2a31a1a06c', 1112736, 1669360711, 1669360366),
+	(12, 1669360732, 1, '2', '/user_upload/_default/images/coffee/pariwat-pannium-WLk7wdUpKXc-unsplash.jpg', '3421de25f86ea6505a4f38f5e2282d8966a0e22d', '5fcdd6b05f77f18c38c3a5de68e97d04b609cfd1', 'jpg', 'image/jpeg', 'pariwat-pannium-WLk7wdUpKXc-unsplash.jpg', 'e54bc8da5e68760318c4097435ef64d02a7c900d', 736524, 1669360711, 1669360326),
+	(13, 1669360732, 1, '2', '/user_upload/_default/images/coffee/samedy-nguon-DCcF-mJtTlA-unsplash.jpg', '5dd36a973b4325326724d5983a0b6e5ecd2373a3', '5fcdd6b05f77f18c38c3a5de68e97d04b609cfd1', 'jpg', 'image/jpeg', 'samedy-nguon-DCcF-mJtTlA-unsplash.jpg', 'd18e7fc28ebf73b228a1bcc17c3fecfd861e09c7', 1913521, 1669360711, 1669360427),
+	(14, 1669360732, 1, '2', '/user_upload/_default/images/coffee/tabitha-turner-3n3mPoGko8g-unsplash.jpg', 'c2198b793c46529000077b924d925e5d4c6f81cc', '5fcdd6b05f77f18c38c3a5de68e97d04b609cfd1', 'jpg', 'image/jpeg', 'tabitha-turner-3n3mPoGko8g-unsplash.jpg', '5ededc30d14718f0b66b96e942517dfbff4c16f4', 257710, 1669360711, 1669360271)
+;
+
 
 INSERT INTO `sys_file_metadata` (`uid`, `pid`, `file`, `sys_language_uid`, `l10n_parent`, `title`, `description`, `alternative`, `tstamp`, `crdate`, `width`, `height`)
 VALUES
 	(1, 0, 1, 0, 0, NULL, 'Ein gesunder Nadelwald in dichtem Nebel.', 'Nadelwald im Nebel', 1645446736, 1645436840, 6000, 4000),
 	(2, 0, 2, 0, 0, NULL, 'Ein entlaubter Baum beim Winter-Sonnenuntergang', 'Entlaubter Baum im Winter', 1645446856, 1645436840, 4128, 2752),
 	(3, 0, 3, 0, 0, NULL, 'Eine Aufnahme der Meeresoberfläche mit den Umrissen eines Tieres unter Wasser', 'Meerwasser mit Tierumgrisse', 1645446971, 1645436840, 2624, 3936),
-	(4, 0, 4, 0, 0, NULL, 'Ein Spaziergänger macht einen ausführlichen Spaziergang im verschneiten Wald', 'Spaziergang im Winterwald', 1645447062, 1645436840, 6000, 4000);
+	(4, 0, 4, 0, 0, NULL, 'Ein Spaziergänger macht einen ausführlichen Spaziergang im verschneiten Wald', 'Spaziergang im Winterwald', 1645447062, 1645436840, 6000, 4000),
+	(5, 0, 5, 0, 0, NULL, 'Viele kleine Kaffeebohnen', 'Kaffeebohnen fallen in den Kaffee', 1669360728, 1669360728, 4000, 6000),
+	(6, 0, 6, 0, 0, NULL, NULL, NULL, 1669360728, 1669360728, 4480, 6720),
+	(7, 0, 7, 0, 0, NULL, NULL, NULL, 1669360728, 1669360728, 3696, 5544),
+	(8, 0, 8, 0, 0, NULL, NULL, NULL, 1669360728, 1669360728, 6185, 4123),
+	(9, 0, 9, 0, 0, NULL, NULL, NULL, 1669360728, 1669360728, 4480, 6720),
+	(10, 0, 10, 0, 0, NULL, NULL, NULL, 1669360728, 1669360728, 3087, 4631),
+	(11, 0, 11, 0, 0, NULL, NULL, NULL, 1669360728, 1669360728, 3089, 2048),
+	(12, 0, 12, 0, 0, NULL, NULL, NULL, 1669360728, 1669360728, 2810, 4219),
+	(13, 0, 13, 0, 0, NULL, NULL, NULL, 1669360728, 1669360728, 2880, 3840),
+	(14, 0, 14, 0, 0, NULL, NULL, NULL, 1669360728, 1669360728, 3000, 3996)
+;
+
---
+++ craft/data/demo-sql/nessa/030_page_ce_portfolio_file_collections.sql
@@ -0,0 +1,166 @@
+-- noinspection SqlNoDataSourceInspectionForFile
+
+###
+# Example page of CE-Portfolio
+###
+INSERT INTO `pages` SET
+	pid = @sitemap_level_1,
+	doktype = 1,
+	title = 'CE-Portfolio mit Dateisammlung',
+	slug = CONCAT(@slug_example_ce_de, '/ce-portfolio-file-collections'),
+	seo_title = 'Beispielhafte Inhalte mit den Inhaltselement Portfolio mit Dateisammlung(en)'
+;
+SET @page = LAST_INSERT_ID();
+
+INSERT INTO `pages` SET
+	pid = @page,
+	sys_language_uid = @language_en,
+	l10n_parent = @page,
+	l10n_source = @page,
+	doktype = 1,
+	slug = CONCAT(@slug_example_ce_en, '/ce-portfolio-file-collections'),
+
+	title = 'CE-Portfolio with file collection(s)',
+	seo_title = 'Examples of content with element Portfolio with file collection(s)'
+;
+
+
+##
+# get image
+##
+SET @file_image_1 = (SELECT uid FROM sys_file WHERE identifier LIKE "/user_upload/_default/images/coffee/ante-samarzija-lsmu0rUhUOk-unsplash.jpg" LIMIT 1);
+SET @file_image_2 = (SELECT uid FROM sys_file WHERE identifier LIKE "/user_upload/_default/images/coffee/jeanie-de-klerk-hmLY7GiNFyE-unsplash.jpg" LIMIT 1);
+SET @file_image_3 = (SELECT uid FROM sys_file WHERE identifier LIKE "/user_upload/_default/images/coffee/jeremy-yap-jn-HaGWe4yw-unsplash.jpg" LIMIT 1);
+SET @file_image_4 = (SELECT uid FROM sys_file WHERE identifier LIKE "/user_upload/_default/images/coffee/nathan-dumlao-l59fmhtprIE-unsplash.jpg" LIMIT 1);
+SET @file_image_5 = (SELECT uid FROM sys_file WHERE identifier LIKE "/user_upload/_default/images/coffee/nathan-dumlao-mgxgvwam_-c-unsplash.jpg" LIMIT 1);
+SET @file_image_6 = (SELECT uid FROM sys_file WHERE identifier LIKE "/user_upload/_default/images/coffee/nathan-dumlao-nBJHO6wmRWw-unsplash.jpg" LIMIT 1);
+
+
+###
+# Example content 1
+###
+INSERT INTO `sys_file_collection` SET
+  pid = @page,
+  title = 'Dateisammlung 1 - mit manuell ausgewählten Dateien',
+  type = 'static',
+  files = 6,
+  storage = 0,
+  folder = ''
+;
+SET @example_file_collection = LAST_INSERT_ID();
+
+INSERT INTO `sys_file_reference` (`pid`, `uid_local`, `uid_foreign`, `table_local`, `tablenames`, `fieldname`, `crop`)
+VALUES
+	(@page, @file_image_1, @example_file_collection, 'sys_file', 'sys_file_collection', 'files', ''),
+	(@page, @file_image_2, @example_file_collection, 'sys_file', 'sys_file_collection', 'files', ''),
+	(@page, @file_image_3, @example_file_collection, 'sys_file', 'sys_file_collection', 'files', ''),
+	(@page, @file_image_4, @example_file_collection, 'sys_file', 'sys_file_collection', 'files', ''),
+	(@page, @file_image_5, @example_file_collection, 'sys_file', 'sys_file_collection', 'files', ''),
+	(@page, @file_image_6, @example_file_collection, 'sys_file', 'sys_file_collection', 'files', '')
+;
+
+INSERT INTO `sys_file_collection` SET
+	pid = @page,
+	sys_language_uid = @language_en,
+	l10n_parent = @example_file_collection,
+  title = 'File collection 1 - with manuell selected files',
+  type = 'static',
+  files = 6,
+  storage = 0,
+  folder = ''
+;
+SET @example_file_collection_overlay = LAST_INSERT_ID();
+
+INSERT INTO `sys_file_reference` (`pid`, `uid_local`, `uid_foreign`, `table_local`, `tablenames`, `fieldname`, `crop`)
+VALUES
+	(@page, @file_image_1, @example_file_collection_overlay, 'sys_file', 'sys_file_collection', 'files', ''),
+	(@page, @file_image_2, @example_file_collection_overlay, 'sys_file', 'sys_file_collection', 'files', ''),
+	(@page, @file_image_3, @example_file_collection_overlay, 'sys_file', 'sys_file_collection', 'files', ''),
+	(@page, @file_image_4, @example_file_collection_overlay, 'sys_file', 'sys_file_collection', 'files', ''),
+	(@page, @file_image_5, @example_file_collection_overlay, 'sys_file', 'sys_file_collection', 'files', ''),
+	(@page, @file_image_6, @example_file_collection_overlay, 'sys_file', 'sys_file_collection', 'files', '')
+;
+
+INSERT INTO `tt_content` SET
+	pid = @page,
+	CType = 'nessa_portfolio_file_collections',
+	header = 'Referenzen',
+	subheader = 'Ein Auszug aus unseren Referenzen',
+	header_layout = 2,
+	bodytext = '<p>Proin sapien ipsum, porta a, auctor quis, euismod ut, mi. Nunc nonummy metus. Praesent blandit laoreet nibh. Morbi mollis tellus ac sapien.&nbsp;Pellentesque auctor neque nec urna. Fusce fermentum odio nec arcu. In ac felis quis tortor malesuada pretium. Praesent porttitor, nulla vitae posuere iaculis, arcu nisl dignissim dolor, a pretium mi sem ut ipsum. Cras ultricies mi eu turpis hendrerit fringilla.</p>\r\n<p>Donec vitae orci sed dolor rutrum auctor. Praesent porttitor, nulla vitae posuere iaculis, arcu nisl dignissim dolor, a pretium mi sem ut ipsum. Phasellus dolor. Praesent adipiscing. Nulla sit amet est.</p>',
+	file_collections = @example_file_collection,
+  filelink_sorting = '',
+  filelink_sorting_direction = ''
+;
+SET @example_ce = LAST_INSERT_ID();
+
+INSERT INTO `tt_content` SET
+	pid = @page,
+	t3_origuid = @example_ce,
+	l18n_parent = @example_ce,
+	l10n_source = @example_ce,
+	sys_language_uid = @language_en,
+	CType = 'nessa_portfolio_file_collections',
+	header = 'References',
+	subheader = 'An excerpt from our references',
+	header_layout = 2,
+	bodytext = '<p>Proin sapien ipsum, porta a, auctor quis, euismod ut, mi. Nunc nonummy metus. Praesent blandit laoreet nibh. Morbi mollis tellus ac sapien.&nbsp;Pellentesque auctor neque nec urna. Fusce fermentum odio nec arcu. In ac felis quis tortor malesuada pretium. Praesent porttitor, nulla vitae posuere iaculis, arcu nisl dignissim dolor, a pretium mi sem ut ipsum. Cras ultricies mi eu turpis hendrerit fringilla.</p>\r\n<p>Donec vitae orci sed dolor rutrum auctor. Praesent porttitor, nulla vitae posuere iaculis, arcu nisl dignissim dolor, a pretium mi sem ut ipsum. Phasellus dolor. Praesent adipiscing. Nulla sit amet est.</p>',
+	file_collections = @example_file_collection_overlay,
+  filelink_sorting = '',
+  filelink_sorting_direction = ''
+;
+
+
+###
+# Example content 2
+###
+INSERT INTO `sys_file_collection` SET
+  pid = @page,
+  title = 'Dateisammlung 2 - Auswahl aus Ordner "coffee"',
+  type = 'folder',
+  files = 0,
+  storage = 1,
+  folder = '/user_upload/_default/images/coffee/'
+;
+SET @example_file_collection = LAST_INSERT_ID();
+
+INSERT INTO `sys_file_collection` SET
+	pid = @page,
+	sys_language_uid = @language_en,
+	l10n_parent = @example_file_collection,
+  title = 'File collection 2 - Selection of folder "coffee"',
+  type = 'folder',
+  files = 0,
+  storage = 1,
+  folder = '/user_upload/_default/images/coffee/'
+;
+SET @example_file_collection_overlay = LAST_INSERT_ID();
+
+INSERT INTO `tt_content` SET
+	pid = @page,
+	CType = 'nessa_portfolio_file_collections',
+	header = 'Referenzen',
+	subheader = 'Ein Auszug aus unseren Referenzen',
+	header_layout = 2,
+	bodytext = '<p>Proin sapien ipsum, porta a, auctor quis, euismod ut, mi. Nunc nonummy metus. Praesent blandit laoreet nibh. Morbi mollis tellus ac sapien.&nbsp;Pellentesque auctor neque nec urna. Fusce fermentum odio nec arcu. In ac felis quis tortor malesuada pretium. Praesent porttitor, nulla vitae posuere iaculis, arcu nisl dignissim dolor, a pretium mi sem ut ipsum. Cras ultricies mi eu turpis hendrerit fringilla.</p>\r\n<p>Donec vitae orci sed dolor rutrum auctor. Praesent porttitor, nulla vitae posuere iaculis, arcu nisl dignissim dolor, a pretium mi sem ut ipsum. Phasellus dolor. Praesent adipiscing. Nulla sit amet est.</p>',
+	file_collections = @example_file_collection,
+  filelink_sorting = '',
+  filelink_sorting_direction = ''
+;
+SET @example_ce = LAST_INSERT_ID();
+
+INSERT INTO `tt_content` SET
+	pid = @page,
+	t3_origuid = @example_ce,
+	l18n_parent = @example_ce,
+	l10n_source = @example_ce,
+	sys_language_uid = @language_en,
+	CType = 'nessa_portfolio_file_collections',
+	header = 'References',
+	subheader = 'An excerpt from our references',
+	header_layout = 2,
+	bodytext = '<p>Proin sapien ipsum, porta a, auctor quis, euismod ut, mi. Nunc nonummy metus. Praesent blandit laoreet nibh. Morbi mollis tellus ac sapien.&nbsp;Pellentesque auctor neque nec urna. Fusce fermentum odio nec arcu. In ac felis quis tortor malesuada pretium. Praesent porttitor, nulla vitae posuere iaculis, arcu nisl dignissim dolor, a pretium mi sem ut ipsum. Cras ultricies mi eu turpis hendrerit fringilla.</p>\r\n<p>Donec vitae orci sed dolor rutrum auctor. Praesent porttitor, nulla vitae posuere iaculis, arcu nisl dignissim dolor, a pretium mi sem ut ipsum. Phasellus dolor. Praesent adipiscing. Nulla sit amet est.</p>',
+	file_collections = @example_file_collection_overlay,
+  filelink_sorting = '',
+  filelink_sorting_direction = ''
+;
