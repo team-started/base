e1b006f0cee41383cb8f0d95614f95be2d7e6fe2
Christian Wolfram <c.wolfram@chriwo.de>
2025-03-09 11:22:17 +0100

    [TASK] Migrate Gitlab CI to Github workflows (#1)
    Migrate the Gitlab CI linter jobs to Github workflows.

--- build/.php-cs-fixer.php
+++ build/.php-cs-fixer.php
@@ -4,14 +4,14 @@ $config = \TYPO3\CodingStandards\CsFixerConfig::create();
 $config
     ->getFinder()
     ->in([
-        __DIR__ . '/../../app/backend/packages/customer_sitepackage',
-        __DIR__ . '/../../app/backend/packages/sitepackage_freya',
-        __DIR__ . '/../../app/backend/packages/sitepackage_nessa',
-        __DIR__ . '/../../app/backend/packages/starter',
-        __DIR__ . '/../../app/backend/packages/starter_freya',
-        __DIR__ . '/../../app/backend/packages/starter_nessa',
-        __DIR__ . '/../../app/backend/packages/starter_twig',
-        __DIR__ . '/../../app/backend/packages/starter_twig_news',
+        __DIR__ . '/../app/backend/packages/customer_sitepackage',
+        __DIR__ . '/../app/backend/packages/sitepackage_freya',
+        __DIR__ . '/../app/backend/packages/sitepackage_nessa',
+        __DIR__ . '/../app/backend/packages/starter',
+        __DIR__ . '/../app/backend/packages/starter_freya',
+        __DIR__ . '/../app/backend/packages/starter_nessa',
+        __DIR__ . '/../app/backend/packages/starter_twig',
+        __DIR__ . '/../app/backend/packages/starter_twig_news',
     ])
     ->exclude([
         'frontend',
