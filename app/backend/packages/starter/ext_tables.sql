5eb25d43f09ecdefec30b23ad405cb7dfda57fae
Christian Wolfram <c.wolfram@chriwo.de>
2025-07-04 11:06:16 +0200

    [!!!][TASK] Drop additional asset field in page settings
    Drop the field `tx_starter_nav_media`.

--- app/backend/packages/starter/ext_tables.sql
+++ app/backend/packages/starter/ext_tables.sql
@@ -1,11 +1,4 @@
 #
-# Extend table structure for table 'pages'
-#
-CREATE TABLE pages (
-	tx_starter_nav_media int(11) unsigned DEFAULT '0' NOT NULL
-);
-
-#
 # Extend table structure for table 'tt_content'
 #
 CREATE TABLE tt_content (
